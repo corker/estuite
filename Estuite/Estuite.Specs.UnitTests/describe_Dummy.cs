c85bcf352bcad8296c94b83627c99b7c4fcc708d
mikhailb <mikhailb@fenestrae.com>
2017-02-04 14:57:13 +0100

    initial commit

---
+++ Estuite/Estuite.Specs.UnitTests/describe_Dummy.cs
@@ -0,0 +1,12 @@
+﻿using Shouldly;
+
+namespace Estuite.Specs.UnitTests
+{
+    public class describe_Dummy : nspec
+    {
+        private void when_something()
+        {
+            it["should be true"] = () => 0.ShouldBe(0);
+        }
+    }
+}
---
+++ Estuite/Estuite.Specs.UnitTests/nspec.cs
@@ -0,0 +1,27 @@
+﻿using NSpec;
+using NSpec.Domain;
+using NSpec.Domain.Formatters;
+using NUnit.Framework;
+using System.Linq;
+using Shouldly;
+
+namespace Estuite.Specs.UnitTests
+{
+    [TestFixture]
+    public abstract class nspec : NSpec.nspec
+    {
+        [Test]
+        public void debug()
+        {
+            var currentSpec = GetType();
+            var finder = new SpecFinder(new[] { currentSpec });
+            var filter = new Tags().Parse(currentSpec.Name);
+            var builder = new ContextBuilder(finder, filter, new DefaultConventions());
+            var runner = new ContextRunner(filter, new ConsoleFormatter(), false);
+            var result = runner.Run(builder.Contexts().Build());
+
+            //assert that there aren't any failures
+            result.Failures().Count().ShouldBe(0);
+        }
+    }
+}
