7b4c2556cc14cc92ae5876d7220e3e91d085bba9
mikhailb <mikhailb@fenestrae.com>
2017-02-11 20:45:24 +0100

    more tests

---
+++ Estuite.Specs.UnitTests/describe_UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Shouldly;
+
+namespace Estuite.Specs.UnitTests
+{
+    public class describe_UnitOfWork : nspec
+    {
+        private void before_each()
+        {
+            _bucketId = new BucketId("bucket-id");
+        }
+
+        private void when_create()
+        {
+            act = () => _target = new UnitOfWork(_bucketId, null, null);
+            it["has been created"] = () => _target.ShouldNotBeNull();
+            context["and bucket id is null"] = () =>
+            {
+                before = () => _bucketId = null;
+                it["throws exception"] = expect<ArgumentNullException>(
+                    "Value cannot be null.\r\nParameter name: bucketId"
+                );
+            };
+        }
+
+        private UnitOfWork _target;
+        private BucketId _bucketId;
+    }
+}
