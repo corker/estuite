0a2b2297e436e3a9a57ab598b9ccbd81f4d70541
mikhailb <mikhailb@fenestrae.com>
2017-02-11 21:07:25 +0100

    more tests

---
+++ Estuite.Specs.UnitTests/describe_UnitOfWork_HydrateAggregate.cs
@@ -0,0 +1,37 @@
+﻿using Estuite.Domain;
+using NSpec;
+using Shouldly;
+
+namespace Estuite.Specs.UnitTests
+{
+    [Tag("describe_UnitOfWork")]
+    public class describe_UnitOfWork_HydrateAggregate : nspec
+    {
+        private void before_each()
+        {
+            _bucketId = new BucketId("bucket-id");
+            _aggregate = new FakeICanBeHydrated();
+            _target = new UnitOfWork(_bucketId, null, null);
+        }
+
+        private void when_register_aggregate()
+        {
+            act = () => _target.Hydrate(_aggregate);
+            it["calls aggregate with itself"] = () => _aggregate.HydratedTo.ShouldBeSameAs(_target);
+        }
+
+        private class FakeICanBeHydrated : ICanBeHydrated
+        {
+            public IHydrateEventStreams HydratedTo { get; private set; }
+
+            public void HydrateTo(IHydrateEventStreams streams)
+            {
+                HydratedTo = streams;
+            }
+        }
+
+        private UnitOfWork _target;
+        private BucketId _bucketId;
+        private FakeICanBeHydrated _aggregate;
+    }
+}
--- Estuite.Specs.UnitTests/describe_UnitOfWork_RegisterAggregate.cs
+++ Estuite.Specs.UnitTests/describe_UnitOfWork_RegisterAggregate.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using Estuite.Domain;
+﻿using Estuite.Domain;
 using NSpec;
 using Shouldly;
 
