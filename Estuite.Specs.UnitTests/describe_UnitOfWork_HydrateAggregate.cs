5aa1a5c00571dc7c68555e7256b6c2f5894e3136
mikhailb <mikhailb@fenestrae.com>
2017-02-12 16:31:17 +0100

    more tests

--- Estuite.Specs.UnitTests/describe_UnitOfWork_HydrateAggregate.cs
+++ Estuite.Specs.UnitTests/describe_UnitOfWork_HydrateAggregate.cs
@@ -16,24 +16,33 @@ namespace Estuite.Specs.UnitTests
             _target = new UnitOfWork(_bucketId, null, null, null);
         }
 
-        private void when_register_aggregate()
+        private void when_hydrate_aggregate()
         {
-            act = () => _target.Hydrate(_aggregate);
+            actAsync = async () => await _target.Hydrate(_aggregate);
             it["calls aggregate with itself"] = () => _aggregate.HydratedTo.ShouldBeSameAs(_target);
+        }
+
+        private void when_try_hydrate_aggregate()
+        {
+            actAsync = async () => await _target.TryHydrate(_aggregate);
+            it["calls aggregate with itself"] = () => _aggregate.TryHydratedTo.ShouldBeSameAs(_target);
         }
 
         private class FakeICanBeHydrated : ICanBeHydrated
         {
             public IHydrateStreams HydratedTo { get; private set; }
 
+            public IHydrateStreams TryHydratedTo { get; private set; }
+
             public async Task HydrateTo(IHydrateStreams streams, CancellationToken token = new CancellationToken())
             {
                 HydratedTo = streams;
             }
 
-            public async Task<bool> TryHydrateTo(IHydrateStreams streams, CancellationToken token = new CancellationToken())
+            public async Task<bool> TryHydrateTo(IHydrateStreams streams,
+                CancellationToken token = new CancellationToken())
             {
-                HydratedTo = streams;
+                TryHydratedTo = streams;
                 return true;
             }
         }
